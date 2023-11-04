8d92f764b999b1566eeda8986c73d6969d856470
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2023-11-04 06:45:45 -0700

    Add files via upload

---
+++ add-funcion.py
@@ -0,0 +1,12 @@
+# function definition
+def function():
+#Function body
+    a=int(input("enter the first value="))
+    b=int(input("enter the  second value= "))
+    # a=12
+    # b=20
+
+    c=a+b
+    print(c)
+#function calling
+function()
