3abe4a02e9601beee4be02c90a1c0a658cb24f70
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2023-11-06 08:25:37 -0800

    Add files via upload

---
+++ checknumber.py
@@ -0,0 +1,14 @@
+# program to check if a number is positive,negative or zero
+
+n=0
+
+if (n>0):
+    
+    print("is number positive")
+
+elif(n==0):
+    print("number is zero")
+
+else:
+    print("is number negative")
+    
