2834d3b43f77958dc59ed3f1e8d35ce25307b5c7
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2023-11-08 06:18:00 -0800

    Add files via upload

---
+++ checkprime.py
@@ -0,0 +1,14 @@
+
+# check the number is prime or not 
+num=7
+
+for i in range(2,num):
+    if num%i==0:
+        print(" is not  prime")
+        break
+
+else:
+    print("is prime")
+
+              
+              
