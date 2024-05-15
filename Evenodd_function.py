c7421ec5d046337198225797497e99266fa19c0b
karan <karanbhoknal123@gmail.com>
2024-05-15 07:30:46 +0530

    added the file of to check even odd number is using function

---
+++ Evenodd_function.py
@@ -0,0 +1,15 @@
+
+def evenodd(n):
+    
+    
+
+    if (n % 2) == 0:
+
+        print ("The number is even")
+
+    else:
+
+        print ("The provided number is odd")
+n= int(input ("Enter any number to test whether it is odd or even: "))
+evenodd(n)
+
