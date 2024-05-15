18dc6961fde5306a8e5677129b6dc8e9a56dc149
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2024-05-15 07:48:09 +0530

    Update defprime_function.py
    updated the code

--- defprime.py
+++ defprime.py
@@ -1,14 +1,17 @@
 # check the number is prime or not using function
 
 def prime(n):
-   
+   count=0
     
-    for i in range(2,n):
+    for i in range(1,n+1):
         if n%i==0:
-            print("is not prime")
+           count=count+1
+   if(count==2):
+      
+         print("Number is prime")
                 
     else:
-        print("is prime")
+        print("number is not prime")
 n=int(input("enter the number:"))
 prime(n)
                   
