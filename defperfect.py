87447af158731e6ac006726246fc3a5463a130d7
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2024-11-28 14:49:41 +0530

    Update defperfect.py

--- defperfect.py
+++ defperfect.py
@@ -1,16 +1,14 @@
-def perfect(n):
-    sum_of_divisor=0
-    
-    
-    for i in range(1,n):
-        if n%i==0:
-            sum_of_divisor=sum_of_divisor + i
-        
-    if(sum_of_divisor==n):
-        print(f"is a perfect  number")
+n=int(input("Enter the number is perfect or not"))
+count=0
 
-    else:
-        print(f"is  not perfect  number")
+for i in range(1,n):
+    if n%i==0:
+        count=count+1
+    i=i+1
 
-n=int(input("enter the number:"))
-perfect(n)
+if count==2:
+    print("The provided number is perfect")
+
+else:
+    print("The provide number is not perfect")
+    
