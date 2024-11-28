08b8e33b1b6b6de993011b238a28af52a87008d0
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2024-11-28 14:46:17 +0530

    prime.py
    We can perform & check prime number code by using this way

--- checkprime.py
+++ checkprime.py
@@ -1,14 +1,20 @@
 
 # check the number is prime or not 
-num=7
 
-for i in range(2,num):
-    if num%i==0:
-        print(" is not  prime")
-        break
+n=int(input("Enter the number is prime or not 
+count=0
+i=1
+for i in range(1,n+1):
+        if n%i==0:
+            count=count+1
+        i=i+1
 
-else:
-    print("is prime")
+        if count==2:
+            print("prime number")
+
+        else:
+            print("not prime number")
+
 
               
               
